5d9a592bc4f0e10d81f6b190712092146c1100d5
DraginM <kostya2015ok@yandex.ru>
2025-05-06 00:20:39 +0300

    Исправил bot.py
    Исправил свои тупые очепятки. Теперь оно работает

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_api.utils import ret_random_id
+from vk_api.utils import get_random_id
 
 def write_message(sender, message):
   authorize.method('messages.send', {'user_id': sender, 'message': message, 'random_id': get_random_id()})
 
 token = "vk1.a.92y-_Kt4NuqPY45s73_0FecEPVsFgOu3dmKnzRDRVh0WJt7sya8tiSVqZfl_SMOzv-C8gZTAbiJzjjdhsImI2K0RB7gXbWKxLROpHiyQq9rgR9Ko4CobGsycjajLINleTOYYtvWazH6GU099Tl7x-IbuMsqVJYhbV3zw5VDLt9TqBFhljAvyHGycNxZYFzJNtikhnxGkEk8Ei-W4au2tlQ"
-authorize = vk_api.VkApi(token = toker)
+authorize = vk_api.VkApi(token = token)
 longpoll = VkLongPoll(authorize)
-for ivent in longpool.listen():
+for event in longpoll.listen():
   if event.type == VkEventType.MESSAGE_NEW and event.text:
     reseived_message = event.text
     sender = event.user_id

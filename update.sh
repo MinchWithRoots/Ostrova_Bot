78f764909e81e392fd98d83b70c49f638789a6aa
blvdie <neendtochno@gmail.com>
2025-06-04 01:08:17 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 cd ~/Ostrova_Bot || exit
-echo "[INFO] Pulling latest changes from GitHub..."
 git reset --hard HEAD
 git pull origin main
-
-# Перезапуск сервера через systemd
-echo "[INFO] Restarting service..."
-sudo systemctl restart ostrova-bot.service
+pkill -f app.py
+source venv/bin/activate
+nohup python3 app.py > output.log 2>&1 &
